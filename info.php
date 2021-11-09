0c75e5e1677b6cfc2dd0037547a892fbaa6f8de5
Diana Aguilar <dianaleticia2001@hotmail.com>
2021-11-08 21:25:57 -0600

    Score final

---
+++ info.php
@@ -0,0 +1,30 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "save_the_axo";
+    $score = "12";
+
+    //Conection
+    $conn = new mysqli($servername, $username, $password,$dbname);
+
+    if ($conn->connect_error) {
+        die ("Connection failed: " . $conn->connected_error);
+    }
+
+    $sql1 = "UPDATE game SET score='" . $score."' ORDER BY id DESC LIMIT 1";
+    $result = $conn ->query($sql1);
+
+    if ($conn->connect_error){
+        echo "Failed";
+    } 
+    
+    else {
+        echo "Sucess";
+    }
+    $conn->close();
+
+?>
+
+
+    
